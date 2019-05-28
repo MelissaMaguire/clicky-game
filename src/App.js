d748deb3cc5a6135f738fd54eb9f9a66369dec6c
melissa maguire <melissapaigemaguire98@gmail.com>
2019-05-27 23:46:36 -0700

    yay

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      clicky game
+
+      <img src="https://static.boredpanda.com/blog/wp-content/uploads/2016/10/Cute-sloths-320-580885a0d0510__700.jpg"></img>
+      <img src="https://static.boredpanda.com/blog/wp-content/uploads/2016/10/cute-sloths-101-5807684db1058__700.jpg"></img>
+      <img src="https://static.boredpanda.com/blog/wp-content/uploads/2016/10/cute-sloths-57f269182f5ab__700.jpg"></img>
     </div>
   );
 }
